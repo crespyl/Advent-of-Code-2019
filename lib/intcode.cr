d6fc57b9134736c2eb3c5e377ebdeabcad934bdf
Peter Jacobs <peter@crespyl.net>
2019-12-05 18:25:58 -0600

    refactor, comments

--- lib/intcode.cr
+++ lib/intcode.cr
@@ -1,10 +1,11 @@
-# This module define the "Intcode" interpreter, operating on arrays of Integer
-
+# This module defines the "Intcode" interpreter and several utility functions
+# for dealing with opcodes and parameter handling
 module Intcode
   @@DEBUG = false
 
-  # This class is just a holder for information about an Opcode, including Proc
-  # objects for the implementation and a debug/display function
+  # Each supported opcode is represented as instance of the Opcode class, with
+  # the implementation attached as a Proc, along with a function to generate
+  # simplistic human-readable string for debugging
   class Opcode
     property sym : Symbol
     property size : Int32
@@ -14,72 +15,73 @@ module Intcode
     def initialize(@sym, @size, @impl : Proc(VM, Array(Parameter), Int32), @disasm)
     end
 
+    # We use opcode size to indicate both how far the PC should move after
+    # execution and the number of parameters. Number of parameters is always
+    # size-1 since size includes the instruction itself.
     def num_params
       size - 1
     end
 
-    # Execute this opcode inside this VM (we pass the actual instruction as well
-    # so that the opcode can parse out its addressing modes)
+    # Execute this opcode in the context of the given VM, with the provided
+    # params.  Delagates to the attached Proc.
     def exec(vm, params)
       impl.call(vm, params)
     end
 
-    # Attempt to produce a debug string for this opcode
+    # Return a (more or less) human-readable string describing this instruction
     def debug(vm, params)
       disasm.call(vm, params)
     end
 
-    # Map from mode number to symbols
-    def self.lookup_addressing_mode(m)
-      case m
-      when 0 then :position
-      when 1 then :literal
-      else raise "Unsupported addressing mode #{m}"
-      end
-    end
+    # Static methods
 
-    # Get the opcode and parameters at the given address
-    def self.get_opcode_and_params(vm, address)
-      instr = vm.mem[address]
-      opcode, modes = get_opcode_and_modes(instr)
-      {opcode, modes.map_with_index { |m,i| Parameter.new(m, vm.mem[address + i + 1]) }}
-    end
-
-    # Get the opcode and addressing modes for a given instruction
-    def self.get_opcode_and_modes(instr)
+    # Get the opcode and addressing modes for a given instruction, returns a
+    # tuple with the Opcode instance and an array of addressing mode symbols
+    def self.get_opcode_and_modes(instr : Int32)
       opcode = OPCODES[instr % 100]
       modes = [] of Symbol
 
       instr //= 100 # cut off the opcode so we're left with just the mode part
       while modes.size < opcode.num_params
-        modes << lookup_addressing_mode(instr % 10)
+        modes << Intcode.lookup_addressing_mode(instr % 10)
         instr //= 10
       end
 
       {opcode, modes}
     end
+
+    # Get the opcode and parameters at the given address. Same as
+    # Opcode::get_opcode_and_modes, but parses out the actual parameters from
+    # the VM memory.
+    def self.get_opcode_and_params(vm : VM, address : Int32)
+      instr = vm.mem[address]
+      opcode, modes = get_opcode_and_modes(instr)
+      {opcode, modes.map_with_index { |m,i| Parameter.new(m, vm.mem[address + i + 1]) }}
+    end
   end
 
   # Represents an encoded parameter and its addressing mode
   struct Parameter
-    property val : Int32 # the original value in memory
-    property mode : Symbol
+    property val : Int32   # The original value in memory
+    property mode : Symbol # The addressing mode
 
-    def initialize(@mode, @val)
-    end
+    def initialize(@mode, @val) end
 
+    # Return a debug string indicating the mode and value, used for debug and
+    # disasm
     def debug
       case mode
       when :position then "@#{val}"
       when :literal then "#{val}"
       else
-        "??"
+        "?#{mode}:#{val}"
       end
     end
   end
 
   # This class holds the current state of a running interpreter: the memory
-  # (Array(Int32)), the program counter, and a "halted/running" flag
+  # (Array(Int32)), the program counter, and a "halted/running" flag, and
+  # buffers for input/output values.
   class VM
     # Memory
     property mem : Array(Int32)
@@ -102,9 +104,6 @@ module Intcode
       @mem = mem #Array.new(4096, 0)
       @inputs = [] of Int32
       @outputs = [] of Int32
-      mem.each_with_index do |v, i|
-        @mem[i] = v
-      end
     end
 
     def self.from_file(filename)
@@ -115,7 +114,7 @@ module Intcode
       VM.new(Intcode.read_intcode(str))
     end
 
-    # Get the value of the provided parameter
+    # Get the value of the provided parameter, based on the addressing mode
     def read_param(p : Parameter)
       case p.mode
       when :position then mem[p.val]
@@ -142,18 +141,15 @@ module Intcode
     #   :halted       Executed a HALT instruction
     #
     #   :needs_input  Executed an INPUT instruction, but the input buffer was
-    #                 empty, fill the input buffer and call run again to resume
+    #                 empty, fill the input buffer with send_input and call run
+    #                 again to resume
     #
     #   :pc_range_err PC was moved out of the valid memory range
     def run
       while !@halted && !@needs_input && @pc < mem.size
-        begin
-          opcode, params = Opcode.get_opcode_and_params(self, pc)
-        rescue
-          raise "INVALID OPCODE AT #{pc}: #{mem[pc]}"
-        end
-        if opcode == nil
-        end
+        # fetch the next Opcode and its Parameters
+        opcode, params = Opcode.get_opcode_and_params(self, pc)
+        raise "INVALID OPCODE AT #{pc}: #{mem[pc]}" unless opcode
 
         Intcode.log("%5i %05i: %s" % [pc, mem[pc], opcode.debug(self, params)])
         opcode.exec(self, params)
@@ -168,11 +164,15 @@ module Intcode
       end
     end
 
+    # Add a value to the back of the input buffer
     def send_input(val : Int32)
       inputs << val
       @needs_input = false
     end
 
+    # Remove and return a value from the front of the input buffer.
+    #
+    # If the input buffer is empty, sets the appropriate flag and returns nil
     def get_input
       if inputs.size > 0
         input = inputs.shift
@@ -191,37 +191,42 @@ module Intcode
     end
   end
 
-  # Here we define the mapping from integer to opcode, along with the actual
-  # implementation of each, as Proc objects that get bound to the Opcode
-  # instance during execution
+  # Map from addressing mode digit to symbol
+  def self.lookup_addressing_mode(m)
+    case m
+    when 0 then :position
+    when 1 then :literal
+    else raise "Unsupported addressing mode #{m}"
+    end
+  end
+
+  # Here we define the mapping from integer to Opcode, along with the actual
+  # implementation of each as a Proc
   OPCODES = {
-    1 => Opcode.new(:add,
-                    4,
+    1 => Opcode.new(:add, 4,
                     ->(vm: VM, params: Array(Parameter)) {
                       x, y, dest = params
                       vm.write_param_value(dest, vm.read_param(x) + vm.read_param(y))
-                      vm.pc += 4
+                      vm.pc += params.size+1
                     },
                     ->(vm: VM, params: Array(Parameter)) {
                       "ADD %5s, %5s -> %5s" % params.map { |p| p.debug }
                     }),
-    2 => Opcode.new(:mul,
-                    4,
+    2 => Opcode.new(:mul, 4,
                     ->(vm: VM, params: Array(Parameter)) {
                       x, y, dest = params
                       vm.write_param_value(dest, vm.read_param(x) * vm.read_param(y))
-                      vm.pc += 4
+                      vm.pc += params.size+1
                     },
                     ->(vm: VM, params: Array(Parameter)) {
                       "MUL %5s, %5s -> %5s" % params.map { |p| p.debug }
                     }),
-    3 => Opcode.new(:input,
-                    2,
+    3 => Opcode.new(:input, 2,
                     ->(vm: VM, params: Array(Parameter)) {
                       dest = params.first
                       if input = vm.get_input
                         vm.write_param_value(dest, input)
-                        vm.pc += 2
+                        vm.pc += params.size+1
                       else
                         0
                       end
@@ -229,44 +234,40 @@ module Intcode
                     ->(vm: VM, params: Array(Parameter)) {
                       "IN  -> %5s" % params.map { |p| p.debug }
                     }),
-    4 => Opcode.new(:output,
-                    2,
+    4 => Opcode.new(:output, 2,
                     ->(vm: VM, params: Array(Parameter)) {
                       x = params.first
                       vm.write_output(vm.read_param(x))
-                      vm.pc += 2
+                      vm.pc += params.size+1
                     },
                     ->(vm: VM, params: Array(Parameter)) {
                       "OUT %5s" % params.map { |p| p.debug }
                     }),
-    5 => Opcode.new(:jt,
-                    3,
+    5 => Opcode.new(:jt, 3,
                     ->(vm: VM, params: Array(Parameter)) {
                       x, dest = params
                       if vm.read_param(x) != 0
                         vm.pc = vm.read_param(dest)
                       else
-                        vm.pc += 3
+                        vm.pc += params.size+1
                       end
                     },
                     ->(vm: VM, params: Array(Parameter)) {
                       "JT  %5s, %5s" % params.map { |p| p.debug }
                     }),
-    6 => Opcode.new(:jf,
-                    3,
+    6 => Opcode.new(:jf, 3,
                     ->(vm: VM, params: Array(Parameter)) {
                       x, dest = params
                       if vm.read_param(x) == 0
                         vm.pc = vm.read_param(dest)
                       else
-                        vm.pc += 3
+                        vm.pc += params.size+1
                       end
                     },
                     ->(vm: VM, params: Array(Parameter)) {
                       "JF  %5s, %5s" % params.map { |p| p.debug }
                     }),
-    7 => Opcode.new(:lt,
-                    4,
+    7 => Opcode.new(:lt, 4,
                     ->(vm: VM, params: Array(Parameter)) {
                       x, y, dest = params
                       if vm.read_param(x) < vm.read_param(y)
@@ -274,13 +275,12 @@ module Intcode
                       else
                         vm.write_param_value(dest, 0)
                       end
-                      vm.pc += 4
+                      vm.pc += params.size+1
                     },
                     ->(vm: VM, params: Array(Parameter)) {
                       "LT  %5s, %5s -> %5s" % params.map { |p| p.debug }
                     }),
-    8 => Opcode.new(:eq,
-                    4,
+    8 => Opcode.new(:eq, 4,
                     ->(vm: VM, params: Array(Parameter)) {
                       x, y, dest = params
                       if vm.read_param(x) == vm.read_param(y)
@@ -293,8 +293,7 @@ module Intcode
                     ->(vm: VM, params: Array(Parameter)) {
                       "EQ  %5s, %5s -> %5s" % params.map { |p| p.debug }
                     }),
-    99 => Opcode.new(:halt,
-                     1,
+    99 => Opcode.new(:halt, 1,
                      ->(vm: VM, params : Array(Parameter)) {
                        vm.halted = true
                        1
