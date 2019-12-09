40751c048e781a37d3930fe1179400cf4218ab2e
Peter Jacobs <peter@crespyl.net>
2019-12-08 21:06:48 -0600

    clean up day 1

--- day1/day1.cr
+++ day1/day1.cr
@@ -10,26 +10,21 @@ end
 # expect input file as first parameter
 input = ARGV[0]
 
-# Per Day 1, we get the amount of fuel for a given mass:
-#   divide by 3, round down, then subtract 2
-def fuel_for_mass(mass) : BigInt
-  fuel = (mass // 3) - 2
-  return BigInt.new(fuel)
-end
-
 # Read each line from input and sum the fuel costs
 puts "Part 1"
-puts File.read_lines(input)
-  .reject{ |line| line.empty? }
-  .reduce(BigInt.new(0)) { |sum, line| sum + fuel_for_mass(line.to_f) }
+puts File.read(input)
+  .chomp
+  .split('\n')
+  .map{ |l| l.to_i }
+  .reduce(0) { |s,m| s + m // 3 - 2 }
 
 # Part 2 requires us to do this fuel calculation recursively
-def fuel_for_mass_recursive(mass) : BigInt
-  fuel = fuel_for_mass(mass)
-  extra = fuel_for_mass(fuel)
+def fuel_for_mass_recursive(mass)
+  fuel = mass // 3 - 2
+  extra = fuel // 3 - 2
   while extra > 0
     fuel += extra
-    extra = fuel_for_mass(extra)
+    extra = extra // 3 - 2
   end
   return fuel
 end
@@ -38,51 +33,5 @@ puts "\nPart 2"
 t_start = Time.local
 puts File.read_lines(input)
   .reject { |line| line.empty? }
-  .reduce(0) { |sum, line| sum + fuel_for_mass_recursive(line.to_f) }
-puts "  completed in %s" % (Time.local - t_start).to_s
-
-
-puts "\nPart 2 - fibers"
-t_start = Time.local
-
-jobs = 0
-
-results = Channel(BigInt).new(100)
-queue = Channel(String | Nil).new(100)
-
-# spawn workers
-8.times do
-  spawn {
-    puts "\nworker entering\n"
-    loop do
-      val = queue.receive
-      if val.nil?
-        puts "\nworker exiting\n"
-        break
-      else
-        results.send(fuel_for_mass_recursive(BigInt.new(val)))
-      end
-    end
-  }
-end
-puts "  done spawning workers"
-
-File.read_lines(input)
-  .reject { |line| line.empty? }
-  .in_groups_of(1000) { |group|
-    to_send = group.reject { |l| l.nil? }
-    jobs += to_send.size
-    spawn do
-      to_send.each { |l| queue.send(l) }
-    end
-  }
-
-puts "  done enqueuing lines"
-
-sum = BigInt.new(0)
-jobs.times do
-  sum += results.receive
-end
-
-puts sum
+  .reduce(0) { |sum, line| sum + fuel_for_mass_recursive(line.to_i) }
 puts "  completed in %s" % (Time.local - t_start).to_s
