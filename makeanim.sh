ff5e91948c9658a19badac7ce85ac3cf759a1f01
Peter Jacobs <peter@crespyl.net>
2019-12-16 16:07:28 -0600

    tweak anim script

--- makeanim.sh
+++ makeanim.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 # $1 should be a name template for the frames, eg frames/frame-%08d.ppm
 # $2 should be the output filename
-ffmpeg -r 60 -i "$1" -c:v libvpx -b:v 100k -vf scale=320:-1 -sws_flags neighbor -r 60 $2
+ffmpeg -i "$1" -c:v libvpx -crf 4 -b:v 100M -vf scale=320:-1 -sws_flags neighbor $2
