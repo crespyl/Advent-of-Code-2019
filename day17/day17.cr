fdf6757dc4df86ae031f6da31c0fd828688835f9
Peter Jacobs <peter@crespyl.net>
2019-12-17 02:28:51 -0600

    day 17 path finder

--- day17/day17.cr
+++ day17/day17.cr
@@ -9,7 +9,8 @@ vm = VM2.from_string(prog)
 vm.run
 
 map = vm.output.map(&.chr).join.split('\n').map { |l| l.chars.to_a }
-def safe_get_xy(map,x,y)
+def safe_get_xy(map,pos : Tuple(Int32,Int32))
+  x,y = pos
   if map.size > y && y >= 0
     if map[y].size > x && x >= 0
       return map[y][x]
@@ -18,15 +19,24 @@ def safe_get_xy(map,x,y)
   return '.'
 end
 
-# find intersections
+DIRS = [{0,-1},{1,0},{0,1},{-1,0}]
+def safe_get_dir_from_loc(map,loc,dir)
+  safe_get_xy(map, {loc[0]+DIRS[dir][0],loc[1]+DIRS[dir][1]})
+end
+
+# find intersections and robot
+facings = ['^','>','v','<']
+robot = { {-1,-1}, 0}
 intersections = Hash(Tuple(Int32,Int32), Bool).new(false)
 map.each_with_index do |row,y|
   row.each_with_index do |tile,x|
+    facings.index(tile).try { |f| robot = { {x,y}, f} }
+
     # check each direction to see if all 4 are #s
-    up = safe_get_xy(map, x, y-1)
-    down = safe_get_xy(map, x, y+1)
-    left = safe_get_xy(map, x-1, y)
-    right = safe_get_xy(map, x+1, y)
+    up = safe_get_xy(map, {x, y-1})
+    down = safe_get_xy(map, {x, y+1})
+    left = safe_get_xy(map, {x-1, y})
+    right = safe_get_xy(map, {x+1, y})
 
     if [tile, up,down,left,right].all?('#')
       intersections[{x,y}] = true
@@ -36,6 +46,33 @@ map.each_with_index do |row,y|
 end
 
 puts "Part 1: %i" % intersections.keys.map { |i| i[0] * i[1] }.sum
+
+path = [] of String
+dir_count = 0
+while true
+  # from robot pos, find direction left or right
+  forward = safe_get_dir_from_loc(map,robot[0], (robot[1]))
+  right = safe_get_dir_from_loc(map,robot[0], ((robot[1] + 1) % DIRS.size))
+  left = safe_get_dir_from_loc(map,robot[0], ((robot[1] - 1) % DIRS.size))
+
+  if forward == '#'
+    dir_count += 1
+    robot = { {robot[0][0] + DIRS[robot[1]][0], robot[0][1] + DIRS[robot[1]][1]}, robot[1] }
+  else
+    path << dir_count.to_s if dir_count != 0
+    dir_count = 0
+    if left == '#'
+      path << "L"
+      robot = { robot[0], (robot[1] - 1) % DIRS.size }
+    elsif right == '#'
+      path << "R"
+      robot = { robot[0], (robot[1] + 1) % DIRS.size }
+    else
+      break
+    end
+  end
+end
+puts "Path: %s" % path.join(',')
 
 # P2 by manual examination
 script = "\
